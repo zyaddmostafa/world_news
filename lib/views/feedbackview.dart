a82e98e9afb0f3c955adab6394618d68299b400e
zyaddmostafa <zyadmo026@gmail.com>
2024-08-02 01:33:27 +0300

    create firestore method

--- lib/views/feedbackview.dart
+++ lib/views/feedbackview.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:world_news/constants.dart';
 import 'package:world_news/widget/appicon.dart';
 import 'package:world_news/widget/appname.dart';
@@ -6,9 +8,20 @@ import 'package:world_news/widget/custom_textfield.dart';
 import 'package:world_news/widget/customappbar.dart';
 import 'package:world_news/widget/custombuttom.dart';
 
-class Feedbackview extends StatelessWidget {
+class Feedbackview extends StatefulWidget {
   const Feedbackview({super.key});
   static String id = feedbackid;
+
+  @override
+  State<Feedbackview> createState() => _FeedbackviewState();
+}
+
+class _FeedbackviewState extends State<Feedbackview> {
+  String? name, email, feedback, experince, phone;
+
+  CollectionReference userfeedback =
+      FirebaseFirestore.instance.collection('users feedbacks');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,8 +40,8 @@ class Feedbackview extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: ListView(
-          children: const [
-            SizedBox(
+          children: [
+            const SizedBox(
               height: 25,
             ),
             const Icon(
@@ -36,47 +49,78 @@ class Feedbackview extends StatelessWidget {
               size: 40,
               color: Colors.grey,
             ),
-            Center(
+            const Center(
               child: Text(
                 'Feedback',
                 style: TextStyle(color: kcolor1, fontSize: 25),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             CustomTextfield(
+              onchange: (value) {
+                name = value;
+              },
               hint: 'Type ur name ',
             ),
-            SizedBox(
+            const SizedBox(
               height: 12,
             ),
             CustomTextfield(
+              onchange: (value) {
+                phone = value;
+              },
               hint: 'Mobile phone  ',
             ),
-            SizedBox(
+            const SizedBox(
               height: 12,
             ),
             CustomTextfield(
+              onchange: (value) {
+                email = value;
+              },
               hint: 'email',
             ),
-            SizedBox(
+            const SizedBox(
               height: 12,
             ),
             CustomTextfield(
+              onchange: (value) {
+                feedback = value;
+              },
               hint: 'write ur feedback',
             ),
-            SizedBox(
+            const SizedBox(
               height: 12,
             ),
             CustomTextfield(
+              onchange: (value) {
+                experince = value;
+              },
               hint: 'please tell us about ur experince ',
               maxlines: 4,
             ),
-            SizedBox(
+            const SizedBox(
               height: 40,
             ),
-            Custombuttom(buttomname: 'Submit')
+            GestureDetector(
+              onTap: () {
+                Future<void> getfeedback() {
+                  // Call the user's CollectionReference to add a new user
+                  return userfeedback.add(
+                    {
+                      'name': name,
+                      'phone': phone,
+                      'email': email,
+                      'feedback': feedback,
+                      'about the experince': experince,
+                    },
+                  );
+                }
+              },
+              child: const Custombuttom(buttomname: 'Submit'),
+            )
           ],
         ),
       ),
